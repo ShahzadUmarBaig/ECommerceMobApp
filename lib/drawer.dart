93acf98437fd264258045b716cf0392a1057a352
iShahzadUmar <mirzashahzadahmedbaig@gmail.com>
2020-01-30 20:05:44 +0500

    this is my first commit

---
+++ lib/drawer.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer(this.name); // : super(key: key);
+
+  final name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            child: Container(
+              width: double.infinity,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: 40,
+                    child: Image(
+                      width: 50,
+                      height: 50,
+                      image: AssetImage('assets/images/myPick.png'),
+                    ),
+                  ),
+                  Positioned(
+                    top: 50,
+                    left: 60,
+                    child: Text(
+                      name,
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.black,
+            ),
+          ),
+          ListTile(
+            title: Text("My First Tile"),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:pointofsale/drawer.dart';
+
+void main() => runApp(MaterialApp(
+      theme: ThemeData(primaryColor: Colors.black),
+      debugShowCheckedModeBanner: false,
+      title: 'myPage',
+      home: MyApp(),
+    ));
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  final _username = 'Shahzad Umar Baig';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: Text('Building First App'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+            )
+          ]),
+      drawer: MyDrawer('_username'),
+    );
+  }
+}
